4db1c70f02b292f831fe04c3277c2dbe810076fb
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2022-11-23 22:28:56 -0300

    add

---
+++ 01-Iniciando/11-desafio-troca.js
@@ -0,0 +1,18 @@
+let a = 7
+let b = 94
+
+// depois da troca ... a = 94 e b = 7
+
+// Duas maneiras:
+
+let troca = a
+a = b
+b = troca
+
+//ou 
+
+// [a, b] = [b, a] 
+
+
+console.log(a)
+console.log(b)
