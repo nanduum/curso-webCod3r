575f45654ec4791fc24901c58ccf88ca989e7279
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2022-12-04 20:30:50 -0300

    add

---
+++ 03-JavaScript-Funcao/desafios/03-desafio.js
@@ -0,0 +1,7 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function parametro(base, exp) {
+  console.log(Math.pow(base, exp));
+}
+
+parametro(5, 7);
---
+++ 03-JavaScript-Funcao/desafios/04-desafio.js
@@ -0,0 +1,11 @@
+// 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+// e o resto da divisão destes dois valores.
+
+function div(x, y){
+    console.log('Resultado:', x / y)
+    console.log('Resto:', x % y)
+    
+
+}
+
+div(49 , 7)
