998ba755f1383d3c7f889cf2018e77bff3f7df2f
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2022-12-05 15:49:46 -0300

    att

--- 04-JavaScript-Objeto/funcoesImportantes.js
+++ 04-JavaScript-Objeto/funcoesImportantes.js
@@ -4,30 +4,30 @@ const pessoa = {
     peso: 13
 }
 
-console.log(Object.keys(pessoa))
-console.log(Object.values(pessoa))
-console.log(Object.entries(pessoa))
+console.log(Object.keys(pessoa)) //retorna as chaves do objeto (nome delas)
+console.log(Object.values(pessoa)) //retorna os valores
+console.log(Object.entries(pessoa)) //retorna chaves e valores
 
-Object.entries(pessoa).forEach(([chave, valor]) => {
+Object.entries(pessoa).forEach(([chave, valor]) => { //aqui retorna os valores utilizando o destructuring
     console.log(`${chave}: ${valor}`)
 })
 
-Object.defineProperty(pessoa, 'dataNascimento', {
-    enumerable: true,
-    writable: false,
-    value: '01/01/2019'
-})
+Object.defineProperty(pessoa, 'dataNascimento', { //cria definições para a propriedade
+    enumerable: true, //poderá ser numerada?
+    writable: false, //poderá ser modificada?
+    value: '01/01/2019' 
+})// funciona mais ou menos como o freeze
 
 pessoa.dataNascimento = '01/01/2017'
 console.log(pessoa.dataNascimento)
-console.log(Object.keys(pessoa))
+console.log(Object.keys(pessoa)) // lista a chave do objeto, se o valor de enumerable for false, não listará
 
 // Object.assign (ECMAScript 2015)
 const dest = { a: 1 }
 const o1 = { b: 2 }
 const o2 = { c: 3, a: 4 }
-const obj = Object.assign(dest, o1, o2)
+const obj = Object.assign(dest, o1, o2) // concatena todos os objetos listados
 
-Object.freeze(obj)
+Object.freeze(obj) //freeze não deixa o valor do objeto ser modificado
 obj.c = 1234
 console.log(obj)
