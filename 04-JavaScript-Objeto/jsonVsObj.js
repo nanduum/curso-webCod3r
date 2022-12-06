e54a5297f3d7bdf829edcf3a5f5b037d5a3b93bb
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2022-12-06 09:28:22 -0300

    att

--- 04-JavaScript-Objeto/jsonVsObj.js
+++ 04-JavaScript-Objeto/jsonVsObj.js
@@ -1,7 +1,16 @@
 const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c } }
-console.log(JSON.stringify(obj))
+console.log(JSON.stringify(obj)) //transforma objetos em json
+
+// nota que a função soma será excluida
+// json é apenas textual, só carrega dados
 
 // console.log(JSON.parse("{ a: 1, b: 2, c: 3 }"))
 // console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }"))
+
+//forma correta de escrever um json
+
 console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }'))
 console.log(JSON.parse('{ "a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }'))
+
+// nota que os atributos precisam estar com aspas duplas, numeros, chaves, colchetes 
+// ou booleanos não precisam de aspas duplas
